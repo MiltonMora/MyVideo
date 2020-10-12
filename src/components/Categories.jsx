95638ff652069d0602a01102f479b57a6458d556
MiltonMora <mayloevan@gmail.com>
2020-10-12 12:40:39 +0200

    components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/Categories.scss'
+
+const Categories = ({ children, text }) => (
+    <div className="categories">
+        <h3 className="categories__title">{ text }</h3>
+        {children}
+    </div>
+)
+
+export default Categories;
---
+++ src/containers/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import '../assets/styles/App.scss';
+
+const App = () => {
+    const [ videos, setVideos ] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+        .then(response => response.json())
+        .then(data => setVideos(data))
+    }, []);
+
+    console.log(videos)
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+            <Categories text="Mi lista">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            
+            <Categories text="Tendencias">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            
+            <Categories text="Originales">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+        )
+    };
+
+export default App;
